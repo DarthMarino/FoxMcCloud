f25be0e780013802ff8d44593c4250a75b677a68
Marino Gomez <marinogomez24@gmail.com>
2018-07-08 12:34:19 -0400

    1.2
    Ahora se le agrego un esqueleto de la app, con sus partes y diferentes panels

--- interfaces/PassWordGen.rb
+++ interfaces/PassWordGen.rb
@@ -1,9 +1,19 @@
 require 'fox16'
 include Fox
 
-class HelloWorld < FXMainWindow
+class PasswordGenerator < FXMainWindow
   def initialize(app)
-    super(app, "Hello, World!" , :width => 200, :height => 50)
+    super(app, "Password generator", :width => 400, :height => 200)
+    hFrame1 = FXHorizontalFrame.new(self)
+    chrLabel = FXLabel.new(hFrame1, "Number of characters in password:")
+    chrTextField = FXTextField.new(hFrame1, 4)
+    hFrame2 = FXHorizontalFrame.new(self)
+    specialChrsCheck = FXCheckButton.new(hFrame2, "Include special characters in password")
+    vFrame1 = FXVerticalFrame.new(self, :opts => LAYOUT_FILL)
+    textArea = FXText.new(vFrame1, :opts => LAYOUT_FILL | TEXT_READONLY | TEXT_WORDWRAP)
+    hFrame3 = FXHorizontalFrame.new(vFrame1)
+    generateButton = FXButton.new(hFrame3, "Generate")
+    copyButton = FXButton.new(hFrame3, "Copy to clipboard")
   end
   def create
     super
@@ -11,7 +21,10 @@ class HelloWorld < FXMainWindow
   end
 end
 
-app = FXApp.new
-HelloWorld.new(app)
-app.create
-app.run
+if __FILE__ == $0
+  FXApp.new do |app|
+    PasswordGenerator.new(app)
+    app.create
+    app.run
+  end
+end
